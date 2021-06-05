5acc8d47c4619c19ae1a5e5bfc18dcd26d021c54
triest <triest21@gmail.com>
2021-06-06 00:52:03 +0300

    Add gnerate uniq id for user

---
+++ app/Observers/UserObserver.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\User;
+use Illuminate\Support\Str;
+
+class UserObserver
+{
+    /**
+     * Handle the User "created" event.
+     *
+     * @param  \App\Models\User  $user
+     * @return void
+     */
+    public function created(User $user)
+    {
+        //
+        do
+        {
+            $randomString = Str::random(25);
+            $user_code = User::select(['id'])->where('unique_id', $randomString)->first();
+        }
+        while($user_code);
+
+        $user->unique_id=$randomString;
+        $user->save();
+    }
+
+    /**
+     * Handle the User "updated" event.
+     *
+     * @param  \App\Models\User  $user
+     * @return void
+     */
+    public function updated(User $user)
+    {
+        //
+    }
+
+    /**
+     * Handle the User "deleted" event.
+     *
+     * @param  \App\Models\User  $user
+     * @return void
+     */
+    public function deleted(User $user)
+    {
+        //
+    }
+
+    /**
+     * Handle the User "restored" event.
+     *
+     * @param  \App\Models\User  $user
+     * @return void
+     */
+    public function restored(User $user)
+    {
+        //
+    }
+
+    /**
+     * Handle the User "force deleted" event.
+     *
+     * @param  \App\Models\User  $user
+     * @return void
+     */
+    public function forceDeleted(User $user)
+    {
+        //
+    }
+}
